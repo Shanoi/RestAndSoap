4985a8e6d3a4d11b17ed170b980381c95bb5e966
Shanoi <olivboulet@laposte.net>
2018-03-04 15:07:08 +0100

    It's just the beginning

---
+++ RestAndSoap/JCDLibrary/IVelibsRetriever.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCDLibrary
+{
+    public interface IVelibsRetriever
+    {
+
+        [OperationContract]
+        void getCities();
+
+        [OperationContract]
+        void getDataFromCity(string city);
+
+    }
+}
