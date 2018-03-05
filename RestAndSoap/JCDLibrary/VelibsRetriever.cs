db3c678be4f6e3620814f90ec568fed08999cada
Shanoi <olivboulet@laposte.net>
2018-03-05 15:17:01 +0100

    Basic cache in for the stations

--- RestAndSoap/JCDLibrary/VelibsRetriever.cs
+++ RestAndSoap/JCDLibrary/VelibsRetriever.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,7 +34,7 @@ namespace JCDLibrary
 
             foreach (JObject item in jsonArrayCities)
             {
-                result += (String)item.GetValue("name") + "\n" ;
+                result += (String)item.GetValue("name") + "\n";
             }
 
             return result;
@@ -43,61 +44,74 @@ namespace JCDLibrary
         public string getDataFromCity(string city, string station)
         {
 
-            string data;
+            ObjectCache cache = MemoryCache.Default;
+            string data = cache[city] as string;
 
-            WebRequest request = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=" + city + "&apiKey=7efd1067c82b1c9593faa098b1f7f5ea02cd272e");
 
-            try
-            {
-                WebResponse response = request.GetResponse();
-
-                // Get the stream containing content returned by the server.
-                Stream dataStream = response.GetResponseStream();
-                // Open the stream using a StreamReader for easy access.
-                StreamReader reader = new StreamReader(dataStream);
-                // Read the content.
-                data = reader.ReadToEnd();
-                jsonArrayStation = JArray.Parse(data);
-
-                ArrayList stations = new ArrayList();
-
-                foreach (JObject item in jsonArrayStation)
-                {
-
-                    string name = (String)item.GetValue("name");
-                    if (name.ToUpper().Contains(station.ToUpper()))
-                    {
-                        stations.Add(new Station((String)item.GetValue("name"),
-                            (String)item.GetValue("address"),
-                            (String)item.GetValue("status"),
-                            (int)item.GetValue("available_bike_stands"),
-                            (int)item.GetValue("available_bikes")));
-
-                    }
-                }
-
-                string result = "";
-
-                if (stations.Count == 0)
-                {
-                    return "No station to display";
-                }
-
-                foreach (Station item in stations)
-                {
-
-                    result += item.ToString() + "\n\n";
-
-                }
-
-                return result;
-
-            }
-            catch (Exception)
+            if (data == null)
             {
 
-                return "Wrong city name";
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(10.0);
+
+                WebRequest request = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=" + city + "&apiKey=7efd1067c82b1c9593faa098b1f7f5ea02cd272e");
+
+                try
+                {
+                    WebResponse response = request.GetResponse();
+
+                    // Get the stream containing content returned by the server.
+                    Stream dataStream = response.GetResponseStream();
+                    // Open the stream using a StreamReader for easy access.
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    data = reader.ReadToEnd();
+                    
+                    cache.Set(city, data, policy);
+
+                }
+                catch (Exception)
+                {
+
+                    return "Wrong city name";
+                }
+
             }
+
+            jsonArrayStation = JArray.Parse(data);
+
+            ArrayList stations = new ArrayList();
+
+            foreach (JObject item in jsonArrayStation)
+            {
+
+                string name = (String)item.GetValue("name");
+                if (name.ToUpper().Contains(station.ToUpper()))
+                {
+                    stations.Add(new Station((String)item.GetValue("name"),
+                        (String)item.GetValue("address"),
+                        (String)item.GetValue("status"),
+                        (int)item.GetValue("available_bike_stands"),
+                        (int)item.GetValue("available_bikes")));
+
+                }
+            }
+
+            if (stations.Count == 0)
+            {
+                return "No station to display";
+            }
+
+            string result = "";
+
+            foreach (Station item in stations)
+            {
+
+                result += item.ToString() + "\n\n";
+
+            }
+
+            return result;
             
         }
 
