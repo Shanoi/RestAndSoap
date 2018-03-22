a8907d46bb839d68ad28798958fbc457e903cb93
Shanoi <olivboulet@laposte.net>
2018-03-22 09:21:11 +0100

    GUI Async cleared

--- RestAndSoapTP/ClientGUIAsync/App.cs
+++ RestAndSoapTP/ClientGUIAsync/App.cs
@@ -36,10 +36,6 @@ namespace ClientGUIAsync
         private async void btnSearch_ClickAsync(object sender, EventArgs e)
         {
 
-            /*stations.Clear();
-
-            await getStationDataStation(cBoxVilles.SelectedItem.ToString(), txtStation.Text, listItems);*/
-
             stations.Clear();
 
             stations = await client.getListStationFromCityAsync(cBoxVilles.SelectedItem.ToString(), txtStation.Text);
@@ -51,40 +47,14 @@ namespace ClientGUIAsync
             listItems.SelectedIndex = -1; // This optional line keeps the first item from being selected.
             listItems.SelectedIndexChanged += listItems_SelectedIndexChanged;
 
-            /*Task<List<Station>> task = client.getListStationFromCityAsync(cBoxVilles.SelectedItem.ToString(), txtStation.Text);
-
-            Task continuation = task.ContinueWith(t =>
-            {
-
-                listItems.SelectedIndexChanged -= listItems_SelectedIndexChanged;
-                listItems.DataSource = null;
-                listItems.DisplayMember = "Name";
-                listItems.DataSource = task.Result;
-                //listItems.SelectedIndex = -1; // This optional line keeps the first item from being selected.
-                listItems.SelectedIndexChanged += listItems_SelectedIndexChanged;
-
-            });
-            continuation.Wait();*/
-
-            ////stations = client.getListStationFromCity(cBoxVilles.SelectedItem.ToString(), txtStation.Text).ToList<Station>();
-
-            //string ville = cBoxVilles.SelectedItem.ToString();
-            //string station = txtStation.Text;
-
-            //Task t = Task.Factory.StartNew(() => getStationDataStation(ville, station));
-            //t.Wait();
-
-
         }
 
         private void listItems_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //msgBox.Show(listItems.SelectedItem.ToString());
+
             MsgBox msgBox = new MsgBox();
             msgBox.Text = ((Station)listItems.SelectedItem).Name;
             msgBox.Show(stationToString((Station)listItems.SelectedItem));
-
-
 
         }
 
@@ -98,30 +68,5 @@ namespace ClientGUIAsync
                 + "Available bikes : " + station.Available_bikes;
 
         }
-
-        async Task getStationDataStation(string city, string station, ListBox listItems)
-        {
-
-            try
-            {
-                Task<List<Station>> task = new VelibsRetrieverClient().getListStationFromCityAsync(city, station);
-                if (task == await Task.WhenAny(task, Task.Delay(10000000)))
-                {
-                    listItems.SelectedIndexChanged -= listItems_SelectedIndexChanged;
-                    listItems.DataSource = null;
-                    listItems.DisplayMember = "Name";
-                    listItems.DataSource = await task;
-                    listItems.SelectedIndex = -1; // This optional line keeps the first item from being selected.
-                    listItems.SelectedIndexChanged += listItems_SelectedIndexChanged;
-                }
-                else Console.WriteLine("Timed out");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-        }
-
     }
 }
