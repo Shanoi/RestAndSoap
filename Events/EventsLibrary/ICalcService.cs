5347e3b21e883cc608ca34730bd7ec6db7bb7d7e
Shanoi <olivboulet@laposte.net>
2018-04-04 23:48:43 +0200

    part 1

---
+++ Events/EventsLibrary/ICalcService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsLibrary
+{
+    [ServiceContract(CallbackContract = typeof(ICalcServiceEvents))]
+    public interface ICalcService
+    {
+        [OperationContract]
+        void Calculate(int nOp, double dblNum1, double dblNum2);
+
+        [OperationContract]
+        void SubscribeCalculatedEvent();
+
+        [OperationContract]
+        void SubscribeCalculationFinishedEvent();
+    }
+}
